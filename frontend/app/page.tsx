455c998806ecffc7cfa320bae5e53c04381840d0
Bastien TAKIS <bastientakis@gmail.com>
2024-02-11 11:57:10 +0100

    [FIX] - we need only one function by callback key in socket instance

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,10 @@
 'use client';
 
 import MainPage from "@/app/Components/MainPage";
-import {useEffect, useState} from "react";
-import {io} from "socket.io-client";
-import {TGame} from "../../types/TGame";
-import {TClient} from "../../types/TClient";
+import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
+import { TGame } from "../../types/TGame";
+import { TClient } from "../../types/TClient";
 import LobbyPage from "@/app/Components/LobbyPage";
 import GamePage from "@/app/Components/GamePage";
 
@@ -19,33 +19,40 @@ export default function Home() {
     const [currentPlayer, setCurrentPlayer] = useState<TClient>(null);
 
     useEffect(() => {
-        socket.on('client-kahoot-create', (data: {game: TGame, owner: TClient}) => {
+        socket.off('client-kahoot-create')
+        socket.on('client-kahoot-create', (data: { game: TGame, owner: TClient }) => {
             setCurrentGame(data.game);
             setCurrentPlayer(data.owner);
             setState('lobby');
         });
 
-        socket.on('client-kahoot-join', (data: {game: TGame, player: TClient|null}) => {
+        socket.off('client-kahoot-join')
+        socket.on('client-kahoot-join', (data: { game: TGame, player: TClient | null }) => {
             setCurrentGame(data.game);
             if (data.player != null) setCurrentPlayer(data.player);
             setState('lobby');
         });
 
+        socket.off('client-kahoot-players')
         socket.on('client-kahoot-players', (data: TGame) => {
             setCurrentGame(data);
         });
 
+        socket.off('client-kahoot-start')
         socket.on('client-kahoot-start', handleStartAndRound);
 
+        socket.off('client-kahoot-round')
         socket.on('client-kahoot-round', handleStartAndRound);
 
+        socket.off('client-kahoot-end')
         socket.on('client-kahoot-end', (data: TGame) => {
             setCurrentGame(data);
             setState('home'); // TODO replace with result !
         });
 
+        socket.off('client-kahoot-answer')
         socket.on('client-kahoot-answer', (data: TGame) => {
-            if(data) {
+            if (data) {
                 document.getElementById("FormAnswer")?.classList.add('hidden')
                 alert("votre reponse a bien été comptabilisé")
             }
@@ -54,6 +61,7 @@ export default function Home() {
             }
         });
 
+        socket.off('server-error')
         socket.on('server-error', (data) => {
             alert(data.status + ' : ' + data.msg)
         });
@@ -83,7 +91,7 @@ export default function Home() {
             <div className="flex gap-6">
                 <h1 className="text-4xl"> Kahoot Like </h1>
                 <button className={'border border-2 btn-alert rounded p-2 ' + isHidden()}
-                        onClick={() => leave()}> Leave the game </button>
+                    onClick={() => leave()}> Leave the game </button>
             </div>
             <MainPage socket={socket} state={state} />
             <LobbyPage socket={socket} state={state} currentGame={currentGame} currentPlayer={currentPlayer} />
